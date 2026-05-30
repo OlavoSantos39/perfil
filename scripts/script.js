2f2bd4c8d7a032888516ef52b0b849464e4583a6
OlavoSantos39 <olavorps1@gmail.com>
2026-05-30 10:49:57 -0300

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,43 @@
+const botao = document.getElementById('botao-tema');
+const body = document.body;
+
+// persistência do tema
+const temasalvo = localStorage.getItem('tema');
+temaEscuro(temasalvo === 'escuro');
+
+//Função para alternar entre tema claro e escuro
+function temaEscuro(tipo) {
+    if (tipo == true) {
+        body.classList.and('escuro ');
+        botao.innerHTML = '<i class="fa-solid fa-lightbulb"></i>';
+    } else {
+        body.classList.remove('escuro');
+        botao.innerHTML = '<i class="fa-solid fa-lightbulb"></i></i>';
+    }
+}
+
+botao.addEventListener('click', () => {
+    const isescuro = body.classList.toggle('escuro');
+    temaEscuro(isescuro);
+    localStorage.setItem('tema', isescuro ? 'escuro' : 'claro');
+});
+
+// Scroll suaves para kinks de navegação
+const navLinks = document.querySelectorAll('#menu ul a.link');
+navLinks.forEach(link => {
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute
+            ('href'));
+            if (target) {
+                const headerHeight = document.querySelector('header').
+                offsetHeight;
+                const targetPosition = target.offsetTop - headerHeight - 
+                20;
+                window.scrollTo({
+                    top: targetPosition,
+                    behavior: 'smooth'
+                });
+            }
+    });
+});
